c7df2e339dbd1d68930d8c1df6e2d091645a0d6e
TolgaKmbl <tolgakumbul@gmail.com>
2023-04-09 18:11:16 +0300

    question 287

---
+++ src/test/java/com/tolgakumbul/leetcode/test287/FindDuplicateNumberTest.java
@@ -0,0 +1,30 @@
+package com.tolgakumbul.leetcode.test287;
+
+import com.tolgakumbul.leetcode.question226.InvertBinaryTree;
+import com.tolgakumbul.leetcode.question287.FindDuplicateNumber;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FindDuplicateNumberTest {
+
+    private FindDuplicateNumber findDuplicateNumber;
+
+    @Before
+    public void setUp() {
+        findDuplicateNumber = new FindDuplicateNumber();
+    }
+
+    @Test
+    public void testFindDuplicate(){
+        int duplicate = findDuplicateNumber.findDuplicate(new int[]{1, 3, 4, 2, 2});
+        Assert.assertEquals(2, duplicate);
+    }
+
+    @Test
+    public void testFindDuplicateCase2(){
+        int duplicate = findDuplicateNumber.findDuplicate(new int[]{3,1,3,4,2});
+        Assert.assertEquals(3, duplicate);
+    }
+
+}
