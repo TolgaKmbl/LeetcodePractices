2d5aff52a2dc58d89a26efd105fe93b93b4f7285
TolgaKmbl <tolgakumbul@gmail.com>
2023-09-24 10:59:45 +0300

    question 1359

---
+++ src/test/java/com/tolgakumbul/leetcode/algorithms/test1359/CountAllValidPickupAndDeliveryOptionsTest.java
@@ -0,0 +1,25 @@
+package com.tolgakumbul.leetcode.algorithms.test1359;
+
+import com.tolgakumbul.leetcode.algorithms.question1359.CountAllValidPickupAndDeliveryOptions;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CountAllValidPickupAndDeliveryOptionsTest {
+
+    private CountAllValidPickupAndDeliveryOptions countAllValidPickupAndDeliveryOptions;
+
+    @Before
+    public void setUp() {
+        countAllValidPickupAndDeliveryOptions = new CountAllValidPickupAndDeliveryOptions();
+    }
+
+    @Test
+    public void testShortestPathBinaryMatrix() {
+        Assert.assertEquals(1, countAllValidPickupAndDeliveryOptions.countOrders(1));
+        Assert.assertEquals(6, countAllValidPickupAndDeliveryOptions.countOrders(2));
+        Assert.assertEquals(90, countAllValidPickupAndDeliveryOptions.countOrders(3));
+        Assert.assertEquals(2520, countAllValidPickupAndDeliveryOptions.countOrders(4));
+    }
+
+}
