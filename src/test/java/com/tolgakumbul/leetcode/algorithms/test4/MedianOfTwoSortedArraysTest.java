b3fa902ad23304ec3fc05e7d0ea4b24594c74e1f
TolgaKmbl <tolgakumbul@gmail.com>
2024-06-17 19:58:28 +0300

    question 4

---
+++ src/test/java/com/tolgakumbul/leetcode/algorithms/test4/MedianOfTwoSortedArraysTest.java
@@ -0,0 +1,22 @@
+package com.tolgakumbul.leetcode.algorithms.test4;
+
+import com.tolgakumbul.leetcode.algorithms.question4.MedianOfTwoSortedArrays;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MedianOfTwoSortedArraysTest {
+
+    private MedianOfTwoSortedArrays medianOfTwoSortedArrays;
+
+    @Before
+    public void setUp() {
+        medianOfTwoSortedArrays = new MedianOfTwoSortedArrays();
+    }
+
+    @Test
+    public void testFindMedianSortedArrays() {
+        Assert.assertEquals(2, medianOfTwoSortedArrays.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}), 0);
+        Assert.assertEquals(2.5, medianOfTwoSortedArrays.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}), 0);
+    }
+}
