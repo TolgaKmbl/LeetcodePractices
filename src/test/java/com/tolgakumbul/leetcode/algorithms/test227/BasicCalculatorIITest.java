1610911f11d7c42f055f77c396a06c64b15f23ce
TolgaKmbl <tolgakumbul@gmail.com>
2024-10-08 09:10:39 +0300

    question 227

---
+++ src/test/java/com/tolgakumbul/leetcode/algorithms/test227/BasicCalculatorIITest.java
@@ -0,0 +1,23 @@
+package com.tolgakumbul.leetcode.algorithms.test227;
+
+import com.tolgakumbul.leetcode.algorithms.question227.BasicCalculatorII;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BasicCalculatorIITest {
+
+    private BasicCalculatorII basicCalculatorII;
+
+    @Before
+    public void setUp() {
+        basicCalculatorII = new BasicCalculatorII();
+    }
+
+    @Test
+    public void testCalculate(){
+        Assert.assertEquals(7, basicCalculatorII.calculate("3+2*2"));
+        Assert.assertEquals(1, basicCalculatorII.calculate(" 3/2 "));
+        Assert.assertEquals(5, basicCalculatorII.calculate(" 3+5 / 2 "));
+    }
+}
