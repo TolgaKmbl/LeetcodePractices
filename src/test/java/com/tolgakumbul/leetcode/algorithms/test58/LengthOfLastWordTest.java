e008763e322455d945db40e414955a5bc481ec85
TolgaKmbl <tolgakumbul@gmail.com>
2024-11-17 16:26:44 +0300

    question 58

---
+++ src/test/java/com/tolgakumbul/leetcode/algorithms/test58/LengthOfLastWordTest.java
@@ -0,0 +1,14 @@
+package com.tolgakumbul.leetcode.algorithms.test58;
+
+import com.tolgakumbul.leetcode.algorithms.question58.LengthOfLastWord;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LengthOfLastWordTest {
+
+    @Test
+    public void testLengthOfLastWord(){
+        LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+        Assert.assertEquals(5, lengthOfLastWord.lengthOfLastWord("Hello World"));
+    }
+}
